679663f5842d3d3a9c16ed24fca8f2c7b2bb4bec
Gabriel Henrique de Morais Pires <gabrielhmorais@usp.br>
2022-06-08 23:36:07 -0300

    Método de Liebmann e plotagem

--- parte_2.py
+++ parte_2.py
@@ -3,10 +3,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Discretização
+# delta_phi = 10
+# delta_r = 0.01
 delta_phi = 2
 delta_r = 0.005
 
-def plota_malha(delta_phi, delta_r):
+def resolve_malha(delta_phi, delta_r, tx_sobrerelaxacao, tolerancia):
+
+    # Variáveis que definem o tamanho da matriz que representa a malha (m = no. linhas, n = no. colunas)
+    m = int(40/delta_phi) + 1
+    n = int(0.08/delta_r) + 1
+
+    # Matriz que representa a malha inicializada com zeros 
+    malha = np.zeros((m, n))
+
+    # V conhecido nas bordas esquerda e direita do material A (particularidade azul)
+    malha[:, 0] = 100
+    malha[:, n-1] = 0
+
+    # Variável que mede a diferença dos valores de V para checar a tolerância de convergência
+    maior_diferenca = 0
+
+    while ((maior_diferenca > tolerancia) or maior_diferenca == 0):
+        
+        maior_diferenca = 0 
+
+        # Método de Liebmann
+        for i in list(range(0, m)):       
+            for j in list(range(1, n-1)): # A iteração das colunas não precisa passar na primeira e na última (condições de contorno)  
+                
+                # Potencial na iteração anterior
+                V_velho = malha[i, j]
+
+                # Valor do raio na iteração j
+                r = 0.03 + j*delta_r
+
+                # Particularidades para método
+                if ((1 <= i <= m-1) and (1 <= j <= n-1)):     # Particularidade cor: pontos no centro da matriz -> esse é o caso genérico
+                    V_novo = (malha[i,j+1] + malha[i, j-1])/2 # Coloquei qualquer coisa: isso é uma média na linha
+                    
+                elif (i == 0):                                # Particularidade cor: pontos na primeira linha
+                    V_novo = malha[i+1, j]*r                  # Coloquei qualquer coisa: esse tem um fator de posição
+
+                elif ((i == m)):
+                    V_novo = malha[i-1, j]                    # Coloquei qualquer coisa
+                
+                # Sobre-relaxação
+                malha[i, j] = tx_sobrerelaxacao*V_novo + (1-tx_sobrerelaxacao)*V_velho
+
+                # Checagem da convergência
+                if abs(malha[i, j] - V_velho) > maior_diferenca:
+                     maior_diferenca = abs(malha[i, j] - V_velho)
+
+    return malha
+
+def plota_malha(delta_phi, delta_r, malha, linha_de_grade=True):
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
     # Contorno do material A
@@ -21,9 +72,26 @@ def plota_malha(delta_phi, delta_r):
     ax.plot(np.linspace(0, np.deg2rad(18)), [0.05]*50, color='b')
     ax.plot(np.linspace(0, np.deg2rad(18)), [0.08]*50, color='b',linewidth=1.3)
 
-    # Linhas da malha
-    ax.set_rticks(np.arange(0.03, 0.12, delta_r)) 
-    ax.set_thetagrids(np.arange(0, 40, delta_phi))
+    # Parâmetros da grade
+    r = np.arange(0.03, 0.12, delta_r)
+    theta = np.arange(0, 40, delta_phi)
+
+    # Linhas de grade
+    if linha_de_grade:
+        ax.set_rticks(r) 
+        ax.set_thetagrids(theta)
+        ax.grid(True)
+    if (not linha_de_grade):
+        ax.set_rticks([]) 
+        ax.set_thetagrids([])
+
+    # Plotagem dos valores da malha
+    r = np.linspace(0.03, 0.11, int(0.08/delta_r) + 1)
+    p = np.linspace(np.deg2rad(40), 0, int(40/delta_phi) + 1)
+    R, P = np.meshgrid(r, p)
+
+    cp = ax.contourf(P, R, malha)
+    fig.colorbar(cp)
 
     # Limites do gráfico
     ax.set_thetamin(0)
@@ -38,9 +106,9 @@ def plota_malha(delta_phi, delta_r):
     ax.set_xticklabels([])
     ax.set_yticklabels([])
     
-    ax.grid(True)
-
     #ax.set_title("A line plot on a polar axis", va='bottom')
     plt.show()
 
-plota_malha(delta_phi, delta_r)
+malha = resolve_malha(delta_phi, delta_r, 1.5, 0.01)
+print(malha)
+plota_malha(delta_phi, delta_r, malha, linha_de_grade=True)
